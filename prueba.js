603595143b60b804c7e37b84819c62974064da42
renzo <72679166@continental.edu.pe>
2022-01-24 18:29:24 -0500

    SIMULADOR JAVASCRIPT

--- prueba.js
+++ prueba.js
@@ -1,46 +1,68 @@
+let total=0;
 let opcion = 0;
-let primer_numero= 0;
-let segundo_numero= 0;
-let resultado=0;
-//Repetimos hasta que se ingresa "ESC"
+let juego1=0;
+let juego2=0;
+let juego3=0;
+let juego4=0;
+let juego5=0;
+
+let subJuego1=0;
+let subJuego2=0;
+let subJuego3=0;
+let subJuego4=0;
+let subJuego5=0;
+const JUEGO1= 10;
+const JUEGO2= 33.99;
+const JUEGO3= 17.99;
+const JUEGO4= 41.89;
+const JUEGO5= 14.89;
+function montoTotal(precio){
+    total=Math.round((total+precio)*100)/100;
+    alert("TOTAL: "+total+"€");
+}
+function subTotal(cantidad,precio){
+    let subTotal=0;
+    subTotal=cantidad*precio;
+    return subTotal;
+}
 do{    
-    alert("[1] SUMA\n[2] RESTA\n[3] DIVISIÓN\n[4] MULTIPLICACIÓN\n[5] SALIR\nELIJA UNA OPCIÓN: ");
-    opcion=parseInt(prompt(""));
+    opcion=parseInt(prompt("-----------LISTA DE JUEGOS-----------\n[1]DARK SOULS 3                         10€\n[2]GOD OF WAR                      33.99€\n[3]NIER AUTOMATA                 17.99€\n[4]ELDEN RING                        41.89€\n[5]HORIZON ZERO DAWN       14.89€\n[6]Ver Carrito\n[7]SALIR\nElija una opción: "));
     switch(opcion){
         case 1:{
-            primer_numero=parseInt(prompt("Ingrese primer número:"));
-            segundo_numero=parseInt(prompt("Ingrese segundo número:"));
-            resultado=primer_numero+segundo_numero;
-            alert("La suma  de "+primer_numero+" y "+segundo_numero+" es: "+resultado);
-            break
+            juego1++;
+            montoTotal(JUEGO1);                       
+            break;
         }        
         case 2:{
-            primer_numero=parseInt(prompt("Ingrese primer número:"));
-            segundo_numero=parseInt(prompt("Ingrese segundo número:"));
-            resultado=primer_numero-segundo_numero;
-            alert("La resta de "+primer_numero+" y "+segundo_numero+"  es "+resultado);            
+            juego2++;
+            montoTotal(JUEGO2);            
             break;
         }        
         case 3:{
-            primer_numero=parseInt(prompt("Ingrese primer número:"));
-            segundo_numero=parseInt(prompt("Ingrese segundo número:"));
-            resultado=primer_numero/segundo_numero;
-            alert("La división de "+primer_numero+" y "+segundo_numero+"  es "+resultado);
+            juego3++;
+            montoTotal(JUEGO3);
             break;
         }
         case 4:{
-            primer_numero=parseInt(prompt("Ingrese primer número:"));
-            segundo_numero=parseInt(prompt("Ingrese segundo número:"));
-            resultado=primer_numero*segundo_numero;
-            alert("La multiplicación de "+primer_numero+" y "+segundo_numero+"  es "+resultado);
+            juego4++;
+            montoTotal(JUEGO4);;
             break;
         }
         case 5:{
+            juego5++;
+            montoTotal(JUEGO5);
+            break;
+        }
+        case 6:{
+            alert("Desc.                                    Cant.       Precio.\nDARK SOULS 3                       "+juego1+"               "+subTotal(juego1,JUEGO1)+"€\nGOD OF WAR                         "+juego2+"               "+subTotal(juego2,JUEGO2)+"€\n[NIER AUTOMATA                  "+juego3+"                "+subTotal(juego3,JUEGO3)+"€\nELDEN RING                           "+juego4+"               "+subTotal(juego4,JUEGO4)+"€\nHORIZON ZERO DAWN         "+juego5+"                "+subTotal(juego5,JUEGO5)+"€\n                                                 TOTAL     "+total+"€");
+            break;
+        }
+        case 7:{            
             alert("GRACIAS");
             break;
         }        
     }    
-    if(opcion<1||opcion>5){
+    if(opcion<1||opcion>7){
         alert("OPCIÓN NO VALIDA")
     }
-}while(opcion != "5" )
+}while(opcion != 7 )
