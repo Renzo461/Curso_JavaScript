7be1dd7b1dafe782cf5f237f546b2004b9e6b49c
renzo <72679166@continental.edu.pe>
2022-02-08 18:16:03 -0500

    MODIFICANDO HTML

--- prueba.js
+++ prueba.js
@@ -1,36 +1,36 @@
-let juegosStock = [
-    {id: 1,nombre: "DARK SOULS 3",plataforma: "Steam",precio: 10},    
-    {id: 2,nombre: "GOD OF WAR",plataforma: "Steam",precio: 34},
-    {id: 3,nombre: "NIER AUTOMATA",plataforma: "Steam",precio: 18},
-    {id: 4,nombre: "ELDEN RING",plataforma: "Steam",precio: 42},
-    {id: 5,nombre: "HORIZON ZERO DAWN",plataforma: "Steam",precio: 15},
-]
-let carrito=[]
-let opcion= 0;
+class juego{
+    constructor(id,nombre,plataforma,precio){
+        this.id=id
+        this.nombre=nombre
+        this.plataforma=plataforma
+        this.precio=precio
+    }
+}
+let juegosStock=[]
 
-function venta(id){
-    let juego=juegosStock.find((juego)=>juego.id==id)
-    carrito.push(juego)
-    montoVenta()
+function agregarJuego(){
+    let id=juegosStock.length+1
+    let nombre=prompt("Ingrese nombre del juego:")
+    let plataforma=prompt("Ingrese plataforma del juego: ")
+    let precio=parseInt(prompt("Ingrese precio del juego:"))
+    const JUEGO= new juego(id,nombre,plataforma,precio)
+    
+    juegosStock.push(JUEGO)
 }
-function montoVenta(){
-    let monto= 0;
-    carrito.forEach(juego=>{
-        monto+=juego.precio
-    })
-    console.log(carrito)
-    console.log("Cantidad de Productos "+carrito.length)
-    console.log("Precio Total: "+monto)
-}
-do{
-    opcion=prompt("["+(juegosStock[0].id)+"]"+"     "+juegosStock[0].nombre+"\n["+(juegosStock[1].id)+"]"+"     "+juegosStock[1].nombre+"\n["+(juegosStock[2].id)+"]"+"     "+juegosStock[2].nombre+"\n["+(juegosStock[3].id)+"]"+"     "+juegosStock[3].nombre+"\n["+(juegosStock[4].id)+"]"+"     "+juegosStock[4].nombre+"\n["+(juegosStock.length+1)+"]"+"     Salir")
-    if (opcion>=1&&opcion<=5){
-        venta(opcion)        
+let op=0
+do{    
+    op=parseInt(prompt("[1] Agregar Juego\n[2] Salir"))
+    switch(op){
+        case 1:{
+            agregarJuego()
+            let indice=juegosStock.length-1
+            document.getElementById("juego").innerHTML += '<tr><td>'+juegosStock[indice].id+'</td><td>'+juegosStock[indice].nombre+'</td><td>'+juegosStock[indice].plataforma+'</td><td>'+juegosStock[indice].precio+'</td></tr>'
+            break
+        }
+        case 2:{
+            alert("SALIENDO")
+            break
+        }
     }
-    else if(opcion==6){
-        alert("Gracias")
-    }
-    else{
-        alert("Opcion Incorrecta")
-    }
-}while(opcion!=6)
+    
+}while(op!=2)
