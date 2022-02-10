598a9900199a408edf80d24a231fc093988ee55f
renzo <72679166@continental.edu.pe>
2022-02-10 17:16:42 -0500

    Events

--- prueba.js
+++ prueba.js
@@ -1,36 +1,29 @@
-class juego{
-    constructor(id,nombre,plataforma,precio){
-        this.id=id
-        this.nombre=nombre
-        this.plataforma=plataforma
-        this.precio=precio
-    }
-}
-let juegosStock=[]
-
-function agregarJuego(){
-    let id=juegosStock.length+1
-    let nombre=prompt("Ingrese nombre del juego:")
-    let plataforma=prompt("Ingrese plataforma del juego: ")
-    let precio=parseInt(prompt("Ingrese precio del juego:"))
-    const JUEGO= new juego(id,nombre,plataforma,precio)
-    
+function agregarJuego() {
+    const id = juegosStock.length + 1
+    const nombre = document.getElementById("nombre").value
+    const plataforma = document.getElementById("plataforma").value
+    const precio = document.getElementById("precio").value
+    const JUEGO = new juego(id, nombre, plataforma, precio)
+    console.log(nombre)
     juegosStock.push(JUEGO)
+
 }
-let op=0
-do{    
-    op=parseInt(prompt("[1] Agregar Juego\n[2] Salir"))
-    switch(op){
-        case 1:{
-            agregarJuego()
-            let indice=juegosStock.length-1
-            document.getElementById("juego").innerHTML += '<tr><td>'+juegosStock[indice].id+'</td><td>'+juegosStock[indice].nombre+'</td><td>'+juegosStock[indice].plataforma+'</td><td>'+juegosStock[indice].precio+'</td></tr>'
-            break
-        }
-        case 2:{
-            alert("SALIENDO")
-            break
-        }
-    }
-    
-}while(op!=2)
+const inNombre=document.getElementById("inputNombre"),
+inImagen=document.getElementById("inputImagen"),
+inPrecio=document.getElementById("inputPrecio"),
+outNombre=document.getElementById("outputNombre"),
+outImagen=document.getElementById("outputImagen"),
+outPrecio=document.getElementById("outputPrecio")
+
+inNombre.addEventListener("keyup",()=>{
+    outNombre.innerText=inNombre.value
+})
+inImagen.addEventListener("keyup",()=>{
+    outImagen.src=inImagen.value
+})
+inPrecio.addEventListener("keyup",()=>{
+    outPrecio.innerText="€ "+inPrecio.value
+})
+inPrecio.addEventListener("change",()=>{
+    outPrecio.innerText="€ "+inPrecio.value
+})
