d721fc77605c468962a15ddd07330db40d61f8da
renzo <72679166@continental.edu.pe>
2022-02-22 20:08:47 -0500

    OPTIMIZANDO

--- prueba.js
+++ prueba.js
@@ -1,51 +1,105 @@
-let juegosStock = [{ id: 1, nombre: "Age of Empires", precio: 40, descuento: 0.5, imagen: "./Juegos/ageOfEmpiresiv.jpg" },
-{ id: 2, nombre: "Beyond Good Evil", precio: 40, descuento: 0.5, imagen: "./Juegos/beyondGoodEvi.jpg" },
-{ id: 3, nombre: "Blue Protocol", precio: 40, descuento: 0.5, imagen: "./Juegos/bluProtocol.jpg" },
-{ id: 4, nombre: "Dead Island 2", precio: 40, descuento: 0.5, imagen: "./Juegos/deadIsland2.jpg" },
-{ id: 5, nombre: "Elden Ring", precio: 40, descuento: 0.5, imagen: "./Juegos/eldenRring.jpg" },
-{ id: 6, nombre: "FarCry 6", precio: 40, descuento: 0.5, imagen: "./Juegos/farcry6.jpg" },
-{ id: 7, nombre: "Forza Horizon 5", precio: 40, descuento: 0.5, imagen: "./Juegos/forzahorizon5.jpg" },
-{ id: 8, nombre: "Hitman 3", precio: 40, descuento: 0.5, imagen: "./Juegos/hitman3.jpg" },
-{ id: 9, nombre: "Little Nightmares 2", precio: 40, descuento: 0.5, imagen: "./Juegos/littlenightmares2.jpg" },
-{ id: 10, nombre: "Medium", precio: 40, descuento: 0.5, imagen: "./Juegos/medium.jpg" }]
-let stock = document.querySelector(".juegos");
-let venta = document.querySelector(".venta")
-let cadenaStock = ""
-let cadenaVenta = ""
-juegosStock.forEach(juego => {
-  cadenaStock += '<div class="juego"><div class="imagen"><img src="' + juego.imagen + '" alt="JUEGO"><div class="precio"><p>$' + juego.precio + '</p></div>'
-  if (juego.descuento == 0) {
-    cadenaStock += '</div><div class="texto"><h1>' + juego.nombre + '</h1></div><div class="boton"><button type="button" class="comprar" id="' + juego.id + '">COMPRAR</button></div></div>';
+class JUEGO {
+  constructor(id, nombre, precio, descuento, imagen) {
+    this.idJuego = "J" + id
+    this.nombre = nombre
+    this.precio = precio
+    this.descuento = descuento
+    if (descuento == 0) {
+      this.precioPromocional = 0
+    }
+    else {
+      this.precioPromocional = this.precio * (1 - descuento)
+    }
+    this.imagen = imagen
   }
-  else {
-    cadenaStock += '<div class="descuento"><div class="contenedor"><p>' + (juego.descuento * 100) + '%</p></div></div></div><div class="texto"><h1>' + juego.nombre + '</h1></div><div class="boton"><button type="button" class="comprar" id="' + juego.id + '">COMPRAR</button></div></div>'
-  }
-})
-stock.innerHTML += cadenaStock;
-let boton = []
-let carrito = []
-let prueba = JSON.parse(localStorage.getItem("Carrito"))
-if (prueba != null) {
-  prueba.forEach(juego => {
-    carrito.push(juego)
-    cadenaVenta += '<div class="ventaJuego"><div class="ventaNombre"><p>' + juego.nombre + '</p></div><div class="ventaPrecio"><p>$' + juego.precio + '</p></div><div class="ventaBoton"><button class="eliminar">Eliminar</button></div></div>'
-  })
 }
-venta.innerHTML=cadenaVenta
-function guardarCarrito(clave, valor) {
-  localStorage.setItem(clave, JSON.stringify(valor))
+class VENTA {
+  constructor(id,idJuego, nombre, precio, precioPromocional, cantidad) {
+    this.idVenta="V"+id
+    this.idJuego = idJuego
+    this.nombre = nombre
+    this.precio = precio
+    this.precioPromocional = precioPromocional
+    this.cantidad = cantidad
+    if (this.precioPromocional == 0) {
+      this.subTotal = this.cantidad * this.precio
+    }
+    else {
+      this.subTotal = this.cantidad * this.precioPromocional
+    }
+  }
 }
 
-for (let i = 1; i <= juegosStock.length; i++) {
-  boton[i] = document.getElementById(i)
-  boton[i].addEventListener("click", () => {
-    let compra = juegosStock.find(juego => juego.id == i)
-    carrito.push(compra)
-    guardarCarrito("Carrito", carrito)
-    cadenaVenta = ""
-    carrito.forEach(juego=>{      
-      cadenaVenta +='<div class="ventaJuego"><div class="ventaNombre"><p>'+juego.nombre+'</p></div><div class="ventaPrecio"><p>$'+juego.precio+'</p></div><div class="ventaBoton"><button class="eliminar">Eliminar</button></div></div>'
-    }) 
-    venta.innerHTML=cadenaVenta
+let stock=document.getElementById("listaJuegos")
+let venta = document.getElementById("detallePintar")
+let juegosStock = []
+let boton = []
+let carrito = []
+juegosStock.push(new JUEGO((juegosStock.length + 1), "Age of Empires IV", 60, 0.2, "./Juegos/ageOfEmpiresiv.jpg"))
+juegosStock.push(new JUEGO((juegosStock.length + 1), "Beyond Good and Evil 2", 70, 0, "./Juegos/beyondGoodEvi.jpg"))
+juegosStock.push(new JUEGO((juegosStock.length + 1), "Blue Protocol", 50, 0.15, "./Juegos/bluProtocol.jpg"))
+juegosStock.push(new JUEGO((juegosStock.length + 1), "Dead Island 2", 20, 0, "./Juegos/deadIsland2.jpg"))
+juegosStock.push(new JUEGO((juegosStock.length + 1), "Elden Ring", 60, 0, "./Juegos/eldenRring.jpg"))
+juegosStock.push(new JUEGO((juegosStock.length + 1), "FarCry 6", 60, 0.1, "./Juegos/farcry6.jpg"))
+juegosStock.push(new JUEGO((juegosStock.length + 1), "Forza Horizon 5", 60, 0.1, "./Juegos/forzahorizon5.jpg"))
+juegosStock.push(new JUEGO((juegosStock.length + 1), "Hitman 3", 60, 0, "./Juegos/hitman3.jpg"))
+juegosStock.push(new JUEGO((juegosStock.length + 1), "Little Nightmares 2", 30, 0.25, "./Juegos/littlenightmares2.jpg"))
+juegosStock.push(new JUEGO((juegosStock.length + 1), "Medium", 50, 0.2, "./Juegos/medium.jpg"))
+pintarJuegos(juegosStock);
+
+//AGREGAR CARRITO
+stock.addEventListener("click",e=>{
+  if(e.target.classList.contains("juego-botonVenta")){
+    let compra = juegosStock.find(juego => juego.idJuego == (e.target.id))
+    let verificar = carrito.indexOf(carrito.find(juego => juego.id == (e.target.id)))
+    if (verificar == -1) {
+      carrito.push(new VENTA((carrito.length+1),compra.idJuego, compra.nombre, compra.precio, compra.precioPromocional, 1))
+    }
+    else {
+      carrito[verificar].cantidad++
+      if (carrito[verificar].precioPromocional != 0) {
+        carrito[verificar].subTotal = carrito[verificar].cantidad * carrito[verificar].precioPromocional
+      }
+      else {
+        carrito[verificar].subTotal = carrito[verificar].cantidad * carrito[verificar].precio
+      }
+    }       
+    pintarCarrito(carrito)
+  }
+})
+/
+//ELIMINAR CARRITO
+venta.addEventListener("click",e=>{
+  if(e.target.classList.contains("eliminar")){
+    let indice=carrito.indexOf(carrito.find(venta=>venta.idVenta==(e.target.id)))
+    carrito.splice(indice,1)
+    pintarCarrito(carrito)
+  }
+})
+
+//FUNCTION
+function pintarJuegos(juego) {
+  let cadena = ""
+  juego.forEach(juego => {
+    if (juego.descuento == 0) {
+      cadena += '<div class="juego"><div class="juego-logo"><img src="' + juego.imagen + '" alt=' + juego.nombre + '><div class="juego-logo-precio"><p>$' + juego.precio + '</p></div></div><button id="' + juego.idJuego + '" class="juego-botonVenta">COMPRAR</button></div>';
+    }
+    else {
+      cadena += '<div class="juego"><div class="juego-logo"><img src="' + juego.imagen + '" alt=' + juego.nombre + '><div class="juego-logo-descuento"><div class="juego-logo-descuento-contenedor"><p>-' + (juego.descuento * 100) + '%</p></div></div><div class="juego-logo-precio"><p>$' + juego.precioPromocional + '</p></div></div><button id="' + juego.idJuego + '" class="juego-botonVenta">COMPRAR</button></div>';
+    }
   })
+  stock.innerHTML=cadena
+}
+
+function pintarCarrito(juego) {
+  let cadena = ""
+  juego.forEach(juego=>{
+    if (juego.precioPromocional != 0) {
+      cadena += '<div class="detalle"><div class="detalle-nombre"><p>' + juego.nombre + '</p></div><div class="detalle-precio"><div class="detalle-precio-antes"><p>$' + juego.precio + '</p></div><div class="detalle-precio-ahora"><p>$' + juego.precioPromocional + '</p></div></div><div class="detalle-cantidad"><button class="menos">-</button><p>' + juego.cantidad + '</p><button class="mas">+</button></div><div class="detalle-subTotal"><p>$' + juego.subTotal + '</p></div><div class="detalle-eliminar"><button id="'+juego.idVenta+'" class="eliminar">X</button></div></div>'
+    }
+    else {
+      cadena += '<div class="detalle"><div class="detalle-nombre"><p>' + juego.nombre + '</p></div><div class="detalle-precio"><div class="detalle-precio-ahora"><p>$' + juego.precio + '</p></div></div><div class="detalle-cantidad"><button class="menos">-</button><p>' + juego.cantidad + '</p><button class="mas">+</button></div><div class="detalle-subTotal"><p>$' + juego.subTotal + '</p></div><div class="detalle-eliminar"><button id="'+juego.idVenta+'" class="eliminar">X</button></div></div>'
+    }
+  })  
+  venta.innerHTML=cadena
 }
